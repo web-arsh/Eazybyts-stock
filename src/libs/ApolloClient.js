d1ef6eb32a220e0a5713c4540bfae77c478af51c
Hammad <159513856+web-arsh@users.noreply.github.com>
2025-09-23 11:06:45 +0530

    Update ApolloClient.js

--- src/libs/ApolloClient.js
+++ src/libs/ApolloClient.js
@@ -1,8 +1,13 @@
 import { HttpLink, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
+  const isServer = typeof window === "undefined";
+  const uri = isServer
+    ? process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/graphql"
+    : "/api/graphql";
+
   return new ApolloClient({
-    link: new HttpLink({ uri: "http://localhost:3000/api/graphql" }),
+    link: new HttpLink({ uri }),
     cache: new InMemoryCache(),
   });
 };
